76e37f21394d6abd41d52eb7ae119aa6a38375c7
swlee9508 <stkt12345@gmail.com>
2020-01-10 17:55:46 +0900

    Initial Commit

---
+++ WinterProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WinterProject
+//
+//  Created by 이상욱 on 2020/01/10.
+//  Copyright © 2020 이상욱. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
